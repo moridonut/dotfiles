24e23bad404129e6e40f4bdf8ab11da212e60617
Takayuki Mori <takayuki.mori@syncron.com>
2018-02-22 18:24:30 +0900

    clear logs

--- linux_installation/vim_setup.sh
+++ linux_installation/vim_setup.sh
@@ -1,9 +1,9 @@
-mkdir ~/.vim$
-mkdir ~/.vim/colors$
-mkdir ~/Documents$
-mkdir ~/Documents/vimfiles$
-mkdir ~/Documents/vimfiles/tmp$
+mkdir ~/.vim
+mkdir ~/.vim/colors
+mkdir ~/Documents
+mkdir ~/Documents/vimfiles
+mkdir ~/Documents/vimfiles/tmp
 $
-cp ../colors/*.vim ~/.vim/colors$
-cp ../_vimrc ~/.vimrc$
+cp ../colors/*.vim ~/.vim/colors
+cp ../_vimrc ~/.vimrc
 
