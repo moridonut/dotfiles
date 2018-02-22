a51b28f0a08a4f0e376ca737a5961a189a50d008
Takayuki Mori <takayuki.mori@syncron.com>
2018-02-22 22:51:32 +0900

    Finalize linux installation

--- linux_installation/vim_setup.sh
+++ linux_installation/vim_setup.sh
@@ -1,9 +1,11 @@
 mkdir ~/.vim
 mkdir ~/.vim/colors
+mkdir ~/.vim/bundle
 mkdir ~/Documents
 mkdir ~/Documents/vimfiles
 mkdir ~/Documents/vimfiles/tmp
-$
-cp ../colors/*.vim ~/.vim/colors
+
+cp ../vimfiles/colors/*.vim ~/.vim/colors
 cp ../_vimrc ~/.vimrc
 
+cp -R ../vimfiles/bundle/* ~/.vim/bundle
