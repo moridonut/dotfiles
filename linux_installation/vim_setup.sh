577f733f155a648c2da357fcb9466f68334f60f4
Takayuki Mori <takayuki.mori@syncron.com>
2018-02-23 13:02:41 +0900

    Modifying linux script to make symbolic links

--- linux_installation/vim_setup.sh
+++ linux_installation/vim_setup.sh
@@ -1,11 +1,7 @@
-mkdir ~/.vim
-mkdir ~/.vim/colors
-mkdir ~/.vim/bundle
 mkdir ~/Documents
 mkdir ~/Documents/vimfiles
 mkdir ~/Documents/vimfiles/tmp
 
-cp ../vimfiles/colors/*.vim ~/.vim/colors
-cp ../_vimrc ~/.vimrc
+ln -s ~/dotfiles/vimfiles ~/.vim
+ln -s ~/dotfiles/_vimrc ~/.vimrc
 
-cp -R ../vimfiles/bundle/* ~/.vim/bundle
